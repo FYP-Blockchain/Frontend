e23057804999d3bfebc5e466e43af7d9d5bb8206
Tharindu <tharindugeethanjan@gmail.com>
2025-08-20 19:27:37 +0530

    integrate ticket issuance

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import eventReducer from '../features/events/eventSlice';
+import ticketReducer from '../features/ticket/ticketSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     events: eventReducer,
+    ticket: ticketReducer
   },
 });
