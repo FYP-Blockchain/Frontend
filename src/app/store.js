ae77ae107a5a50764cf0130e424bcb4992ed2b17
Tharindu <tharindugeethanjan@gmail.com>
2025-09-16 22:16:02 +0530

    metamask integartion #8

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import eventReducer from '../features/events/eventSlice';
 import ticketReducer from '../features/ticket/ticketSlice';
+import walletReducer from '../features/wallet/walletReducer';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     events: eventReducer,
-    ticket: ticketReducer
+    ticket: ticketReducer,
+    wallet: walletReducer,
   },
 });
